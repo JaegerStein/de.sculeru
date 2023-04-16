8a056637a9dc9ebdca35f8a1f89ac86105648b52
Simon Maurer <maurer.simon@live.de>
2023-04-16 15:19:09 +0200

    work on entry

--- www/src/script/Entry.js
+++ www/src/script/Entry.js
@@ -1,2 +1,22 @@
-export {};
+export class Entry {
+    constructor(title, id, entryType, content, last) {
+        this.title = title;
+        this.id = id;
+        this.entryType = entryType;
+        this.content = this.formatContent(content);
+        this.timestamp = last ? this.formatTime(last) : null;
+    }
+    formatContent(content) {
+        const formatObsidian = (md) => {
+            return md;
+        };
+        const formatHTML = (html) => {
+            return html;
+        };
+        return this.entryType === 'markdown' ? formatObsidian(content) : formatHTML(content);
+    }
+    formatTime(time) {
+        return time.toString();
+    }
+}
 //# sourceMappingURL=Entry.js.map
--- www/src/script/Entry.ts
+++ www/src/script/Entry.ts
@@ -4,3 +4,36 @@ export interface EntryData {
     last: number,
     last_readable: string
 }
+
+export type EntryType = 'markdown' | 'html';
+
+export class Entry {
+
+    public readonly content: string;
+    public readonly timestamp: string | null;
+
+    public constructor(public readonly title: string,
+                       public readonly id: string,
+                       private readonly entryType: EntryType,
+                       content: string,
+                       last?: number) {
+        this.content = this.formatContent(content);
+        this.timestamp = last ? this.formatTime(last) : null;
+    }
+
+    private formatContent(content: string): string {
+        const formatObsidian = (md: string): string => {
+            return md;
+        }
+
+        const formatHTML = (html: string): string => {
+            return html;
+        }
+
+        return this.entryType === 'markdown' ? formatObsidian(content) : formatHTML(content);
+    }
+
+    private formatTime(time: number): string {
+        return time.toString();
+    }
+}
