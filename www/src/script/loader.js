9848d7ac6f5cebc58efc3421a8e3c760ce32db81
Simon Maurer <maurer.simon@live.de>
2023-04-21 16:43:30 +0200

    working on file loader

---
+++ www/src/script/loader.js
@@ -0,0 +1,26 @@
+import { KB_Category } from "./types.js";
+async function loadInternalURL(url) {
+    const response = await fetch(url);
+    if (!response.ok)
+        throw new Error(`Failed to load internal URL: ${url}`);
+    return await response.text();
+}
+async function loadIndex(category) {
+    const response = await fetch(categoryToPath(category) + 'index.json');
+    if (!response.ok)
+        throw new Error(`Failed to load ${category} index`);
+    return await response.json();
+}
+function categoryToPath(category) {
+    switch (category) {
+        case KB_Category.JOURNAL:
+            return './kb/Journal/';
+        case KB_Category.LORE:
+            return './kb/Legende/';
+        case KB_Category.RULES:
+            return './kb/Regeln/';
+        default:
+            return './tools/';
+    }
+}
+//# sourceMappingURL=loader.js.map
---
+++ www/src/script/loader.ts
@@ -0,0 +1,26 @@
+import {KB_Category, KB_Entry} from "./types.js";
+
+async function loadInternalURL(url: string): Promise<string> {
+    const response: Response = await fetch(url);
+    if (!response.ok) throw new Error(`Failed to load internal URL: ${url}`);
+    return await response.text();
+}
+
+async function loadIndex(category: KB_Category): Promise<KB_Entry[]> {
+    const response: Response = await fetch(categoryToPath(category) + 'index.json');
+    if (!response.ok) throw new Error(`Failed to load ${category} index`);
+    return await response.json();
+}
+
+function categoryToPath(category: KB_Category): string {
+    switch (category) {
+        case KB_Category.JOURNAL:
+            return './kb/Journal/';
+        case KB_Category.LORE:
+            return './kb/Legende/';
+        case KB_Category.RULES:
+            return './kb/Regeln/'
+        default:
+            return './tools/';
+    }
+}
