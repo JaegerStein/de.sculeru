42d9913ba069fdde505394242ec30269921b3c1d
Simon Maurer <maurer.simon@live.de>
2023-04-21 15:56:13 +0200

    added InternalLink class

--- www/src/script/InternalLink.js
+++ www/src/script/InternalLink.js
@@ -1,10 +1,39 @@
 import { A } from "./utils.js";
+import { KB_Category } from "./types.js";
 export default class InternalLink {
-    constructor(filepath) {
-        this.filepath = filepath;
+    constructor(text, relpath, category) {
+        this.text = text;
+        this.relpath = relpath;
+        this.category = category;
+        this.fullLink = () => {
+            let link = './';
+            switch (this.category) {
+                case KB_Category.JOURNAL:
+                    link += 'kb/Journal/';
+                    break;
+                case KB_Category.LORE:
+                    link += 'kb/Legende/';
+                    break;
+                case KB_Category.RULES:
+                    link += 'kb/Regeln/';
+                    break;
+                case KB_Category.TOOLS:
+                    link += 'tools/';
+                    break;
+            }
+            return link + this.relpath;
+        };
+    }
+    toString() {
+        return `InternalLink(text: "${this.text}", relpath: "${this.relpath}", category: "${this.category}")`;
+    }
+    toJSON() {
+        return { text: this.text, relpath: this.relpath, category: this.category };
     }
     toHTML() {
-        const link = A(this.filepath);
+        const link = A(this.fullLink());
+        link.classList.add('internal-link');
+        link.textContent = this.text;
         return link;
     }
 }
--- www/src/script/InternalLink.ts
+++ www/src/script/InternalLink.ts
@@ -1,12 +1,68 @@
 import {A} from "./utils.js";
+import {KB_Category} from "./types.js";
 
+/** Represents an internal link to a knowledge base entry */
 export default class InternalLink {
 
-    public constructor(private readonly filepath: string) {}
+    /**
+     * Creates a new InternalLink instance
+     * @param text - The text content of the link element.
+     * @param relpath - The relative path to the entry in its category.
+     * @param category - The knowledge base category.
+     */
+    public constructor(private readonly text: string,
+                       private readonly relpath: string,
+                       private readonly category: KB_Category) {}
 
+    /**
+     * Returns a string representation of this InternalLink instance.
+     * @returns A String that includes the text, relative path, and category of this instance.
+     */
+    public toString(): string {
+        return `InternalLink(text: "${this.text}", relpath: "${this.relpath}", category: "${this.category}")`;
+    }
+
+    /**
+     * Returns a JSON representation of this InternalLink instance.
+     * @returns An object with the text, relpath, and category properties.
+     */
+    public toJSON(): { text: string, relpath: string, category: KB_Category } {
+        return {text: this.text, relpath: this.relpath, category: this.category};
+    }
+
+    /**
+     * Returns an HTML representation of this link. The full path is automatically resolved from the relative path
+     * and the category.
+     * @returns This link as an HTMLElement
+     */
     public toHTML(): HTMLElement {
-        const link: HTMLElement = A(this.filepath);
-
+        const link: HTMLElement = A(this.fullLink());
+        link.classList.add('internal-link');
+        link.textContent = this.text;
         return link;
+    }
+
+    /**
+     * Resolves the full URL of the link for use in the application
+     * @returns A string representation of the full link URL
+     * @private
+     */
+    private fullLink = (): string => {
+        let link = './';
+        switch (this.category) {
+            case KB_Category.JOURNAL:
+                link += 'kb/Journal/';
+                break
+            case KB_Category.LORE:
+                link += 'kb/Legende/';
+                break
+            case KB_Category.RULES:
+                link += 'kb/Regeln/';
+                break;
+            case KB_Category.TOOLS:
+                link += 'tools/';
+                break;
+        }
+        return link + this.relpath;
     }
 }
