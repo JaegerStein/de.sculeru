fae3c88b1844904cb6a790efaa940271bf96fa0e
Simon Maurer <maurer.simon@live.de>
2023-04-29 16:59:00 +0200

    reworked indexer to include file type

--- indexer.py
+++ indexer.py
@@ -1,26 +1,32 @@
 import os
 import json
-from datetime import datetime
 
 base_path = './www/'
-kbs = ['kb/Legende/', 'kb/Journal/', 'kb/Regeln/', 'tools/']
-kb2cat = {'kb/Legende/': 'lore', 'kb/Journal/': 'journal', 'kb/Regeln/': 'rules', 'tools/': 'tools'}
+kbs = {'kb/Legende/': 'lore', 'kb/Journal/': 'journal', 'kb/Regeln/': 'rules', 'tools/': 'tools'}
 
 index = []
-for kb in kbs:
-    kb_path = os.path.join(base_path, kb)
-    for root, dirs, files in os.walk(kb_path):
+for kb_path, category in kbs.items():
+    full_path = os.path.join(base_path, kb_path)
+    for root, dirs, files in os.walk(full_path):
+        # excludes designated directories from the search
         dirs[:] = [d for d in dirs if not d.endswith('.obsidian')]
         for file in files:
-            if file.endswith('.md') or file.endswith('.html') or file.endswith('.canvas'):
-                file_path = os.path.join(root, file).replace('\\', '/')
+            if file.endswith('.md'):
+                file_type = 'markdown'
+            elif file.endswith('.html'):
+                file_type = 'html'
+            elif file.endswith('.canvas'):
+                file_type = 'canvas'
+            else:
+                continue
+            file_path = os.path.join(root, file).replace('\\', '/')
 
-                uid = file_path.replace(base_path, '')
-                title = os.path.splitext(os.path.basename(file_path))[0]
-                last = int(os.path.getmtime(file_path))
+            uid = file_path.replace(base_path, '')
+            title = os.path.splitext(os.path.basename(file_path))[0]
+            last = int(os.path.getmtime(file_path))
 
-                entry = {'id': uid, 'title': title, 'last': last, 'category': kb2cat[kb]}
-                index.append(entry)
+            entry = {'id': uid, 'title': title, 'last': last, 'category': category, 'type': file_type}
+            index.append(entry)
 
 index_file = os.path.join(base_path, 'index.json')
 with open(index_file, 'w') as f:
