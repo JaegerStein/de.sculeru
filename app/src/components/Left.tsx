2613221409c5f0d1465337a4a6383c8cce3ce7c9
Simon Maurer <maurer.simon@live.de>
2023-05-02 22:17:51 +0200

    base design is back, baby

---
+++ app/src/components/Left.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Left: React.FC = () => {
+    return (
+        <>
+            <h1>Hallo</h1>
+        </>
+    );
+};
+
+export default Left;
--- app/src/index.tsx
+++ app/src/index.tsx
@@ -1,22 +1,21 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './assets/styles/style.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {el} from "./utils/utils";
+import Left from "./components/Left";
+import Right from "./components/Right";
 
-const left: HTMLElement = el('left')!;
-const center: HTMLElement = el('river')!;
-const right: HTMLElement = el('right')!;
+const left: HTMLElement = el('left-root')!;
+const center: HTMLElement = el('center-root')!;
+const right: HTMLElement = el('right-root')!;
 
-const root = ReactDOM.createRoot(
-  el('root')!
-);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const strict = (component: React.ReactElement): React.ReactElement => <React.StrictMode>{component}</React.StrictMode>
+
+ReactDOM.render(strict(<Left/>), left);
+ReactDOM.render(strict(<App/>), center);
+ReactDOM.render(strict(<Right/>), right);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
