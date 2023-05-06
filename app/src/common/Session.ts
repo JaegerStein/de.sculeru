d81f885ad8eafbceca496bac274af9c4de231003
Simon Maurer <maurer.simon@live.de>
2023-05-06 13:24:51 +0200

    load session index

--- app/src/common/Session.ts
+++ app/src/common/Session.ts
@@ -3,6 +3,7 @@ import {loadJSON} from "./utils";
 import {IndexEntry} from "./types";
 
 const store: Storage = localStorage;
+const INDEX = 'index';
 
 export default abstract class Session {
 
@@ -12,19 +13,44 @@ export default abstract class Session {
     public static set category(category: Category | null) {
         Session._category = category;
         if (category) store.setItem(CATEGORY, category);
-        else store.removeItem('category');
+        else store.removeItem(CATEGORY);
     }
 
     private static activeCategory(): void { Session.category = store.getItem(CATEGORY) as Category | null; }
 
     /* INDEX */
-    private static _index: Map<string, string[]> = new Map();
+    private static _index: Map<string, IndexEntry> = new Map();
 
     private static async loadIndex(): Promise<void> {
+        Session._index.clear();
         const index: IndexEntry[] = await loadJSON('index.json');
-        index.forEach((entry: IndexEntry) => {
-            console.log(entry);
-        });
+        for (const entry of index) Session._index.set(entry.title, entry);
+        store.setItem(INDEX, JSON.stringify(index));
+        console.log(Session.lore);
+    }
+
+    private static filterEntries(category: Category): IndexEntry[] {
+        const entries: IndexEntry[] = [];
+        for (const entry of Session._index.values())
+            if (entry.category === category) entries.push(entry);
+        entries.sort((a, b) => a.title.localeCompare(b.title));
+        return entries;
+    }
+
+    public static get lore(): IndexEntry[] {
+        return Session.filterEntries(Category.LORE);
+    }
+
+    public static get journal(): IndexEntry[] {
+        return Session.filterEntries(Category.JOURNAL);
+    }
+
+    public static get rules(): IndexEntry[] {
+        return Session.filterEntries(Category.RULES);
+    }
+
+    public static get tools(): IndexEntry[] {
+        return Session.filterEntries(Category.TOOLS);
     }
 
     /**
