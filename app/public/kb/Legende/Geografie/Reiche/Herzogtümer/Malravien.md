523766e1891ccf4728971fa7c7808ec15e71c54e
Simon Maurer <maurer.simon@live.de>
2023-05-10 14:44:49 +0200

    refactored entries

--- app/public/kb/Legende/Geografie/Reiche/Herzogtümer/Malravien.md
+++ app/public/kb/Legende/Geografie/Reiche/Herzogtümer/Malravien.md
@@ -1,3 +1,3 @@
-<span class="float-right br4">![[malravien.jpg|400]]</span>
+<span class="float-right br4">![[malravien.jpg|300]]</span>
 **Malravien** (oder die **Niedermark**, wie das Großherzogtum gemeinhin in den [[Siebenlande|Siebenlanden]] genannt wird) ist ein Protektorat des Königreichs [[Padrinal]].
 
--- app/src/components/Entry.tsx
+++ app/src/components/Entry.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Dispatch, ReactNode, SetStateAction, useEffect, useState} from 'react';
 import {ReactComponent as CloseIcon} from "../assets/images/x.svg";
 import '../assets/styles/entry.css';
 import {el, loadText} from "../common/utils";
@@ -6,42 +6,67 @@ import Session from "../common/Session";
 import parse from "html-react-parser";
 import markdownToHTML from "../obsidian";
 import {open} from "../App";
+import {IndexEntry} from "../common/types";
 
 const ENTRY_CLASSNAME = 'entry relative';
 const ENTRY_HEADER_CLASSNAME = 'entry-header flex-row jc-between ai-center';
 const ENTRY_CONTENT_CLASSNAME = 'entry-content';
 
-interface EntryProperties {
+interface EntryProps {
     title: string,
     onRemove: () => void
 }
 
-const Entry: React.FC<EntryProperties> = (properties: EntryProperties) => {
-    const [children, setChildren] = React.useState<React.ReactNode>(<p>Lade Eintrag...</p>);
-    const entryId = 'entry-' + properties.title.replace(' ', '');
-    const removeEntry = () => properties.onRemove();
+const loadChildren = (title: string, setChildren: SetChildren): void => {
+    const indexEntry: IndexEntry | null = Session.entry(title);
+    if (!indexEntry) setChildren(<p>Dieser Eintrag konnte nicht geladen werden</p>)
+    else loadText(indexEntry.id).then((text: string) => {
+        const entryContent = parse(markdownToHTML(text));
+        setChildren(entryContent);
+    });
+}
 
-    React.useEffect(() => {
-        const indexEntry = Session.entry(properties.title);
-        if (!indexEntry) setChildren(<p>Dieser Eintrag konnte nicht geladen werden</p>)
-        else loadText(indexEntry.id).then((text: string) => {
-            setChildren(parse(markdownToHTML(text)));
-        });
-    }, []);
+const enableEntryLinks = (entryId: string): void => {
+    const entry: HTMLElement | null = el(entryId);
+    if (!entry) return;
+    entry.querySelectorAll('a').forEach((link: HTMLAnchorElement) => {
+        link.onclick = (event: MouseEvent) => {
+            event.preventDefault();
+            open(link.getAttribute('href') ?? '');
+        }
+    });
+}
 
-    React.useEffect(() => {
-        el(entryId)?.querySelectorAll('a').forEach((link: HTMLAnchorElement) => {
-            link.onclick = (event: MouseEvent) => {
-                event.preventDefault();
-                open(link.getAttribute('href') ?? '');
-            }
-        });
-    }, [children]);
+const enableImageEnlargement = (entryId: string): void => {
+    const entry: HTMLElement | null = el(entryId);
+    if (!entry) return;
+    entry.querySelectorAll('img').forEach((image: HTMLImageElement) => {
+        image.onclick = () => {
+            const imgURL: string | null = image.getAttribute('src');
+            if (!imgURL) return;
+            const newWindow = window.open(imgURL, '_blank');
+            if (newWindow) newWindow.focus();
+        }
+    });
+}
+
+type Children = ReactNode;
+type SetChildren = Dispatch<SetStateAction<Children>>;
+const Entry: React.FC<EntryProps> = ({title, onRemove}: EntryProps) => {
+    const [children, setChildren]: [Children, SetChildren] = useState<Children>(<p>Lade Eintrag...</p>);
+    const entryId = 'entry-' + title.replace(' ', '');
+    const removeEntry = () => onRemove();
+
+    useEffect(() => loadChildren(title, setChildren), []); // on mount
+    useEffect(() => {
+        enableEntryLinks(entryId);
+        enableImageEnlargement(entryId);
+    }, [children]); // on children change
 
     return (
         <div className={ENTRY_CLASSNAME} id={entryId}>
             <div className={ENTRY_HEADER_CLASSNAME}>
-                <h1>{properties.title}</h1>
+                <h1>{title}</h1>
                 <CloseIcon onClick={removeEntry}/>
             </div>
             <hr/>
